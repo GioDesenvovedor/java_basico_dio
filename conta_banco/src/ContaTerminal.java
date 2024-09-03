ff4e777fdf1871c77449c7fac435672d640ebf51
Giovanni <gio.dev.app@gmail.com>
2024-09-02 23:53:32 -0300

    entrega desafio

--- conta_banco/src/ContaTerminal.java
+++ conta_banco/src/ContaTerminal.java
@@ -1,9 +1,25 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 public class ContaTerminal {
     public static void main(String[] args)  {
        
-      /*  Scanner scanner = new Scanner(System.in);
+     
+   
+      
+      Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+   
+      
+     
+        // Solicita e armazena o nome cliente
+        System.out.println("Por favor, digite o seu nome !");
+       String nomecliente = scanner.next();
+      
+     
+        // Solicita e armazena o nome cliente
+        System.out.println("Por favor, digite o seu nome !");
+       String sobrenome = scanner.next();
        
         // Solicita e armazena o número da agência
         System.out.println("Por favor, digite o número da Agência !");
@@ -15,24 +31,26 @@ public class ContaTerminal {
         Integer numero_conta = scanner.nextInt();
         scanner.nextLine(); 
 
-        // Solicita e armazena o nome cliente
-        System.out.println("Por favor, digite o seu nome !");
-       String nome_cliente = scanner.nextLine();
+        
        
         System.out.println("Por favor, digite o seu Saldo !");
         double saldo = scanner.nextDouble();
 
-       String menssagem = printInput(nome_cliente, agencia, numero_conta, saldo);
+       String menssagem = printInput(nomecliente, sobrenome, agencia, numero_conta, saldo);
 
        System.out.println(menssagem);
        scanner.close();
+     
+
+      
     }
-    public static String printInput(String nomeCliente, String agencia, Integer numeroConta, Double saldo) {
+    public static String printInput(String nomeCliente, String sobrenome,String agencia, Integer numeroConta, Double saldo) {
         // Formata a mensagem com as informações inseridas
         return String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", 
-                             nomeCliente, agencia, numeroConta, saldo);
-    }*/
-    }
+                             nomeCliente, sobrenome, agencia, numeroConta, saldo);
+    
+                             
+    } 
 
 
     
